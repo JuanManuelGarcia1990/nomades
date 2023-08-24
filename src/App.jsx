12919ee39bbb3abb908c93b8b47311cb5a0f68f3
Juan Manuel Garcia <1990garcia.juan.manuel@gmail.com>
2023-08-24 19:24:23 -0300

    siguiendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar/navbar";
 import Button from "./components/Button/button";
+import Home from "./components/Home/home";
 
 function App() {
   return (
     <div>
       <Navbar icono="https://w7.pngwing.com/pngs/225/984/png-transparent-computer-icons-shopping-cart-encapsulated-postscript-shopping-cart-angle-black-shopping.png"/>
       <h1>Nomades Turismo</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo adipisci
-        vel corrupti magnam vero obcaecati animi sit consectetur quod cumque
-        fugiat dolor eos aliquam, minima ipsa dolorem quas labore eum! Ratione
-        ipsam quia odit est delectus esse nisi corrupti nemo voluptatibus
-        laborum minima repellendus quidem, quas consequatur, quasi recusandae
-        explicabo earum ab modi illo maxime sint, incidunt voluptas eius?
-        Repudiandae quasi consequuntur reiciendis ab sint corporis rerum velit
-        ducimus eveniet, aperiam delectus iusto obcaecati iure ut quas
-        praesentium. Necessitatibus, excepturi quas magni commodi quis, sapiente
-        vel enim tempore numquam quam illum iste doloribus totam aliquid
-        architecto eos sequi sed a cum nesciunt! Sit libero ut eos fugiat harum,
-        quos veritatis sequi modi est dolore inventore similique dolorem iure
-        accusamus dolorum veniam nostrum eveniet, perspiciatis aliquam
-        obcaecati, nemo quod quam? Quisquam, reprehenderit harum! Autem expedita
-        aspernatur soluta perferendis adipisci reiciendis, commodi quidem
-        tempora possimus quia ipsum sapiente veritatis repudiandae ea molestiae!
-        Iusto, unde illum.{" "}
-      </p>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      
       <Button texto="home" />
       <Button texto="about" />
       <Button texto="contact" />
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div><h2>Bienvenidos a nomades turismo</h2></div>
+  )
+}
+
+export default Home
