5bb4a20071f1311961fd6043f824571ec841efe8
Juan Manuel Garcia <1990garcia.juan.manuel@gmail.com>
2023-09-05 17:22:38 -0300

    librerias fuera

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,10 +1,5 @@
 import styles from "./header.module.scss";
 import logo from "../../assets/Logo/logo.jpg"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import InstagramIcon from '@mui/icons-material/Instagram';
-
-
 
 
 const Header = () => {
@@ -13,10 +8,7 @@ const Header = () => {
       <img src={logo} />
       <p>Santa Fe 1208 8A Rosario,Santa Fe, Argentina</p>
       <div>
-      <FontAwesomeIcon icon={faEnvelope} />
-      <button
-           endicon= {<InstagramIcon />}>
-      </button>
+
 
       </div>
       <p>Lunes a Sabados 8 a 20hs</p></header>
