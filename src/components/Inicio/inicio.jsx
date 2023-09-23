09ce6e0cdceaa08ee75f1efa29d5535f3a096321
Juan Manuel Garcia <1990garcia.juan.manuel@gmail.com>
2023-09-23 14:17:06 -0300

    carousel

--- src/components/Inicio/inicio.jsx
+++ src/components/Inicio/inicio.jsx
@@ -20,7 +20,7 @@ const Inicio = () => {
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3,
+      items: 1,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -37,22 +37,22 @@ const Inicio = () => {
       <Carousel responsive={responsive}>
         <div className="card">
           <h2>Cataratas</h2>
-          <img className="product--image" src={cataratas} alt="cataratas" />
+          <img className={styles.destinos} src={cataratas} alt="cataratas" />
         </div>
 
         <div className="card">
           <h2>Cordoba</h2>
-          <img className="product--image" src={cordoba} alt="cordoba" />
+          <img className={styles.destinos} src={cordoba} alt="cordoba" />
         </div>
 
         <div className="card">
           <h2>Costa</h2>
-          <img className="product--image" src={costa} alt="costa" />
+          <img className={styles.destinos} src={costa} alt="costa" />
         </div>
 
         <div className="card">
           <h2>Mendoza</h2>
-          <img className="product--image" src={mendoza} alt="mendoza" />
+          <img className={styles.destinos} src={mendoza} alt="mendoza" />
         </div>
       </Carousel>
       <div className={styles.buttonContainer}>
@@ -62,22 +62,22 @@ const Inicio = () => {
       <h2>Internacionales</h2>
       <Carousel responsive={responsive}>
         <div className="card">
-          <img className="product--image" src={brasil} alt="brasil" />
+          <img className={styles.destinos} src={brasil} alt="brasil" />
           <h2>brasil</h2>
         </div>
 
         <div className="card">
-          <img className="product--image" src={caribe} alt="caribe" />
+          <img className={styles.destinos} src={caribe} alt="caribe" />
           <h2>caribe</h2>
         </div>
 
         <div className="card">
-          <img className="product--image" src={eeuu} alt="eeuu" />
+          <img className={styles.destinos} src={eeuu} alt="eeuu" />
           <h2>eeuu</h2>
         </div>
-
+        {/* "product--image" */}
         <div className="card">
-          <img className="product--image" src={peru} alt="peru" />
+          <img className={styles.destinos} src={peru} alt="peru" />
           <h2>peru</h2>
         </div>
       </Carousel>
