e127459ffa58022f69db5d4a35f7d246eaa3f0e6
Juan Manuel Garcia <1990garcia.juan.manuel@gmail.com>
2023-10-02 23:57:22 -0300

    nav

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,17 +2,26 @@ import styles from "./footer.module.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <div className={styles.mainFooter}>
-      <NavLink to="https://www.instagram.com/">
-        <FontAwesomeIcon icon={faInstagram} />
-      </NavLink>
-    </div>
+    <footer className={styles.mainFooter}>
+        <h5>Redes Sociales</h5>
+        <div className={styles.icons}>
+        <NavLink to="https://www.instagram.com/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </NavLink>
+        <NavLink to="https://www.twitter.com/">
+          <FontAwesomeIcon icon={faFacebook} />
+        </NavLink>
+      </div>
+      <div>
+        <p>Sta fe 1813 1B</p>
+      </div>
+    </footer>
   );
 };
 
-library.add(faInstagram);
+library.add(faInstagram, faFacebook);
 export default Footer;
